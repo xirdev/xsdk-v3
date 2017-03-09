af2d847da08c460d8f32aef8ea2283623d1e977d
Ritchie Turner <ritchie@async.cl>
2017-03-09 18:51:00 -0300

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat')
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
+
+
+var jsFiles = 'lib/**/*.js',
+    jsDest = 'quickstart';
+
+gulp.task('xsdk', function() {
+    return gulp.src(jsFiles)
+        .pipe(concat('xsdk.js'))
+        .pipe(gulp.dest(jsDest))
+        .pipe(rename('xsdk.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDest))
+});
