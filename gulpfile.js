ea4230f9e875c50fc0be6021d9d222ef7dc54164
Ritchie Turner <ritchie@async.cl>
2017-03-09 19:51:23 -0300

    ordering

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var uglify = require('gulp-uglify')
 
 
 
-var jsFiles = ["lib/xirsys.core.js", "lib/xirsys.api.js", "lib/xirsys.signal.js", "lib/xirsys.p2p.js", "lib/xirsys.p2p.adapter.js", "lib/xirsys.databind.js", "lib/xirsys.simplewebrtc.connection.js", "lib/xirsys.ui.js", "lib/xirsys.model.js", "lib/xirsys.simplewebrtc.js", "lib.xirsys.quickstart.js"]
+var jsFiles = ["lib/xirsys.core.js", "lib/xirsys.signal.js", "lib/xirsys.api.js", "lib/xirsys.p2p.adapter.js", "lib/xirsys.p2p.js", "lib/xirsys.databind.js", "lib/xirsys.simplewebrtc.connection.js", "lib/xirsys.ui.js", "lib/xirsys.model.js", "lib/xirsys.simplewebrtc.js", "lib.xirsys.quickstart.js"]
 
 jsDest = 'quickstart';
 
@@ -13,7 +13,7 @@ gulp.task('xsdk', function() {
     return gulp.src(jsFiles)
         .pipe(concat('xsdk.js'))
         .pipe(gulp.dest(jsDest))
-        .pipe(rename('xsdk.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(jsDest))
+        // .pipe(rename('xsdk.js'))
+        // .pipe(uglify())
+        // .pipe(gulp.dest(jsDest))
 });
