4a4ec52dee697a54730489c2326e4ce5f621cefe
Ritchie Turner <ritchie@async.cl>
2017-03-09 19:43:51 -0300

    minified ordered

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,16 @@ var rename = require('gulp-rename')
 var uglify = require('gulp-uglify')
 
 
-var jsFiles = 'lib/**/*.js',
-    jsDest = 'quickstart';
+
+var jsFiles = ["lib/xirsys.core.js", "lib/xirsys.api.js", "lib/xirsys.signal.js", "lib/xirsys.p2p.js", "lib/xirsys.p2p.adapter.js", "lib/xirsys.databind.js", "lib/xirsys.simplewebrtc.connection.js", "lib/xirsys.ui.js", "lib/xirsys.model.js", "lib/xirsys.simplewebrtc.js", "lib.xirsys.quickstart.js"]
+
+jsDest = 'quickstart';
 
 gulp.task('xsdk', function() {
     return gulp.src(jsFiles)
         .pipe(concat('xsdk.js'))
         .pipe(gulp.dest(jsDest))
-        // .pipe(rename('xsdk.js'))
-        // .pipe(uglify())
-        // .pipe(gulp.dest(jsDest))
+        .pipe(rename('xsdk.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDest))
 });
